4dd2780e900f29c22264865cde6346d5bb8b0d33
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-10-31 17:09:39 +0100

    feat: updates to header template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <h1>Temp Nav</h1>
-    <nav>
-      <p>
-        <router-link to="/product-overview">Product Overview</router-link>
-        |
-        <router-link to="/product-details">Product Details</router-link>
-        |
-        <router-link to="/wish-lists">Wish Lists</router-link>
-      </p>
-    </nav>
+  <AppHeader :navItems="navItems" class="app__header" />
+
+  <div class="content">
+    <!--    <h1>Temp Nav</h1>-->
+    <!--    <nav>-->
+    <!--      <p>-->
+    <!--        <router-link to="/product-overview">Product Overview</router-link>-->
+    <!--        |-->
+    <!--        <router-link to="/product-details">Product Details</router-link>-->
+    <!--        |-->
+    <!--        <router-link to="/wish-lists">Wish Lists</router-link>-->
+    <!--      </p>-->
+    <!--    </nav>-->
     <router-view></router-view>
   </div>
 </template>
