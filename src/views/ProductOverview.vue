1e246fb86ead1ea21fd41b18cf89cb22219f3faf
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-11-03 11:09:33 +0100

    feat: import list into overview

--- src/views/ProductOverview.vue
+++ src/views/ProductOverview.vue
@@ -1,18 +1,36 @@
 <script lang="ts" setup>
 import useApi from '@/composables/use/api';
 import type { Products } from '@/models/products.model.ts';
+import ProductList from '@/components/ProductList.vue';
 
-const { data: products, loading: isLoading } = useApi<Products>(
-  'http://localhost:3000/products'
-);
+const { data: products } = useApi<Products>('http://localhost:3000/products');
 </script>
 
 <template>
-  <h1>Product Overview</h1>
-  <p>
-    Present a list of all digital products with a search bar to quickly find
-    favorites!
-  </p>
+  <div class="product-overview">
+    <h1 class="product-overview__page-title">
+      Buying is<span class="product-overview__highlight"> believing.</span>
+    </h1>
+    <ProductList :products="products" />
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.product-overview {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &__page-title {
+    text-transform: uppercase;
+    font-family: 'Darker Grotesque', serif;
+    font-size: 3.4rem;
+    margin: 2rem 0;
+  }
+
+  &__highlight {
+    color: $c-pink;
+  }
+}
+</style>
