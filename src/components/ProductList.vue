4f9c77ad351a7991e13d231d59d85ac869cdc182
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-11-03 12:23:01 +0100

    feat: install icons from fontawesome

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,9 +1,23 @@
 <script lang="ts" setup>
 import type { Products } from '@/models/products.model.ts';
+import { ref } from 'vue';
 
 defineProps<{
   products: Products | null;
 }>();
+
+const wishlist = ref<number[]>([]);
+
+const addToWishlist = (id: number) => {
+  wishlist.value.push(id);
+};
+
+const removeFromWishlist = (id: number) => {
+  const index = wishlist.value.indexOf(id);
+  if (index !== -1) {
+    wishlist.value.splice(index, 1);
+  }
+};
 </script>
 
 <template>
@@ -18,6 +32,18 @@ defineProps<{
         {{ product.brand }} - {{ product.name }}
       </h3>
       <p class="products__item-category">{{ product.category }}</p>
+      <i
+        :class="{
+          fas: wishlist.includes(product.id),
+          far: !wishlist.includes(product.id)
+        }"
+        class="fa-heart"
+        @click="
+          wishlist.includes(product.id)
+            ? removeFromWishlist(product.id)
+            : addToWishlist(product.id)
+        "
+      ></i>
     </div>
   </div>
 </template>
@@ -51,6 +77,28 @@ defineProps<{
       font-size: 1.4rem;
       margin: 0.5rem 0;
     }
+
+    .fa-heart {
+      color: #ffffff;
+      transition: color 0.3s ease;
+
+      &.fas {
+        color: #ec0e6e;
+        animation: bounce 0.3s ease;
+      }
+    }
+  }
+}
+
+@keyframes bounce {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
   }
 }
 </style>
