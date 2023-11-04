d2b3804a424377fb5ea48246a9a353c7d77b7b50
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-11-04 23:01:44 +0100

    feat: Adds footer basic setup

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="footer">
+    <section class="footer__container">
+      <div class="footer__quote">
+        "“Are you ready for tech with a bite? Our products are designed to
+        revolutionize the way you work and play. With cutting-edge technology
+        and a commitment to quality, we’re the perfect partner for anyone who
+        wants to stay ahead of the curve. Join the rebellion today and
+        experience the power of tech with a bite!”" <span>ChatGP 4</span>
+      </div>
+      <img class="footer__company-logo" src="@/assets/rebellion.png" />
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  background-color: $c-white;
+
+  &__container {
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  &__quote {
+  }
+
+  &__company-logo {
+  }
+}
+</style>
