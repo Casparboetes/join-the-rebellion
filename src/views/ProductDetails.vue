4cfdd8e62ea2bac198f3dbaab0040ecaf902e79b
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-11-04 17:12:11 +0100

    feat: Update basic setup detail page

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,8 +1,25 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import useApi from '@/composables/use/api.ts';
+import type { Product } from '@/models/products.model.ts';
+
+const route = useRoute();
+const id = parseInt(<string>route.params.id);
+const { data: productDetails } = useApi<Product>(
+  `http://localhost:3000/products/${id}`
+);
+</script>
 
 <template>
   <h1>Product Details</h1>
-  <p>A detailed view of each product is a must-have!</p>
+
+  <div v-if="productDetails">
+    {{ productDetails.brand }}
+    {{ productDetails.name }}
+    {{ productDetails.category }}
+    {{ productDetails.image }}
+    {{ productDetails.specifications }}
+  </div>
 </template>
 
 <style scoped></style>
