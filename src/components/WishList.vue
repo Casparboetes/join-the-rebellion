0283d6b3f1b5d49fcc3adab20260c837c06b8d1d
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-11-05 12:05:17 +0100

    feat: Adds basic setup wish list

---
+++ src/components/WishList.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+import type { WishLists } from '@/models/wish-lists.model.ts';
+import { WishList } from '@/models/wish-lists.model.ts';
+
+defineProps<{
+  wishLists: WishLists | null;
+}>();
+
+const viewList = (id: WishList['id']) => console.log('Hi', id);
+</script>
+
+<template>
+  <div class="wish-list">
+    <div
+      v-for="wishList in wishLists"
+      :key="wishList.id"
+      class="wish-list__item"
+      @click="viewList(wishList.id)"
+    >
+      <h3 class="wish-list__title">
+        {{ wishList.name }}
+      </h3>
+      <div class="wish-list__item-count">
+        {{ wishList.products.length }} products
+      </div>
+      <div class="wish-list__image"></div>
+      <!--      <img-->
+      <!--        :alt="wishList.name"-->
+      <!--        :src="wishlist.image"-->
+      <!--        class="wish-list__item-image"-->
+      <!--      />-->
+      <div class="wish-list__cta">View List</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wish-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 4rem;
+
+  &__item {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.15);
+    cursor: pointer;
+    padding: 2rem;
+    width: calc(100% - 2rem);
+    max-width: 43rem;
+  }
+
+  &__item-count {
+    color: $c-grey;
+    font-weight: bold;
+  }
+
+  &__image {
+    background-color: #666;
+    min-height: 24.6rem;
+    height: auto;
+    width: 100%;
+  }
+
+  &__title {
+    font-size: 1.6rem;
+    font-weight: bold;
+    margin: 0.5rem 0;
+  }
+
+  @include screen($screen-simple) {
+    &__item {
+      width: calc(45%);
+      max-width: 100%;
+    }
+  }
+}
+</style>
--- src/views/WishLists.vue
+++ src/views/WishLists.vue
@@ -1,8 +1,64 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import useApi from '@/composables/use/api';
+import { WishLists } from '@/models/wish-lists.model.ts';
+import WishList from '@/components/WishList.vue';
+
+const { data: wishLists } = useApi<WishLists>(
+  'http://localhost:3000/wishlists'
+);
+</script>
 
 <template>
-  <h1>Wish Lists</h1>
-  <p>Create, delete, and manage them seamlessly!</p>
+  <div class="wish-lists">
+    <h1 class="wish-lists__page-title">
+      The Products You <span class="wish-lists__highlight"> Love.</span>
+    </h1>
+    <hr />
+
+    <WishList :wishLists="wishLists" />
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.wish-lists {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 10rem;
+
+  &__page-title {
+    text-transform: uppercase;
+    font-family: 'Darker Grotesque', serif;
+    font-size: 3rem;
+    margin: 2rem 0;
+  }
+
+  &__highlight {
+    color: $c-pink;
+  }
+
+  @include screen($screen-minimal) {
+    &__page-title {
+      font-size: 4.5rem;
+    }
+  }
+
+  @include screen($screen-simple) {
+    &__page-title {
+      font-size: 7rem;
+    }
+  }
+
+  @include screen($screen-specific) {
+    &__page-title {
+      font-size: 8rem;
+      margin: 0 0 2rem 0;
+    }
+  }
+
+  @include screen($screen-normal) {
+    @include container($w-header);
+  }
+}
+</style>
